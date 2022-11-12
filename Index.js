028b725c237d24cd8e44e4108ca768c06e2f9900
Hiro-Alter <brayan.mena@correounivalle.edu.co>
2022-11-11 23:15:10 -0500

    Prueba 3 :)

--- Index.js
+++ Index.js
@@ -5,7 +5,7 @@ const port = 3000;
 const Hostname = 'localhost'
 
 app.get('/', (req,res) => {
-    res.send('Hola');
+    res.send('A mimir');
 })
 
 app.listen(port, Hostname, function (req, res) {
