886b0c3039db4695c71655d2534ccfb4be88bac1
Hiro-Alter <brayan.mena@correounivalle.edu.co>
2022-11-13 10:42:55 -0500

    aprendiendo...

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const path = require("path");
 app = express();
 
 const port = 3000;
-const Hostname = 'localhost'
+const Hostname = 'localhost';
 
 app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname + "/index.html"));
+    res.sendFile(path.join(__dirname, 'views/index.html'));
 })
 
 app.listen(port, Hostname, function (req, res) {
-    console.log(`http//:${Hostname}:${port}/`)
+    console.log(`http//:${Hostname}:${port}/`);
 })
